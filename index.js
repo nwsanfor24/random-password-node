f180e759b3c922de15a476548056cc48333f6adb
Nathan Sanford <nwsanfor24@gmail.com>
2020-03-07 15:29:07 -0500

    made some changes

--- index.js
+++ index.js
@@ -1,109 +1,56 @@
 var inquirer = require("inquirer");
-
-//Prompts that ask user to specify their password
-function processPassword(answers) {
-    console.log("Your password is:" + answers);
-}
+var fs = require('fs');
 
 //generate the question 
-userQuestion => {
-    inquirer
-        .prompt([
+inquirer.prompt([
+    {
+        type: 'checkbox',
+        message: 'Select your password options',
+        name: 'password',
+        choices: [
             {
-                type: 'checkbox',
-                message: 'Select your password options',
-                name: 'password',
-                choices: [
-                    {
-                        name: 'lowercase'
-                    },
-                    {
-                        name: 'uppercase'
-                    },
-                    {
-                        name: 'number'
-                    },
-                    {
-                        name: 'symbol'
-                    },
-                ],
-            }
-        ])
-        .then(val => {
-            if (val.choice) {
-                this.generatePassword();
-            } else {
-                this.quit();
-            }
-        });
-}
-    
-
-
-class randomFunction {
-    constructor(lower, upper, number, symbol) {
-        this.lower = lower;
-        this.upper = upper;
-        this.number = number;
-        this.symbol = symbol;
+                name: 'lowercase'
+            },
+            {
+                name: 'uppercase'
+            },
+            {
+                name: 'number'
+            },
+            {
+                name: 'symbol'
+            },
+        ],
     }
-}
+]).then(function (data) {
+
+    var filename = data.name.join('') + ".json";
+
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), function (err) {
+
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("Success!");
+    });
+
+});
 
 
 
-//Password functions
-const randomFunction = {
-    lower: randomLower,
-    upper: randomUpper,
-    number: randomNumber,
-    symbol: randomSymbol,
-}
 
-function generatePassword(lower, upper, number, symbol, length) {
-    let generatedPassword = '';
-    const typesCount = lower + upper + number + symbol;
-    const typesArr = [
-        { lower },
-        { upper },
-        { number },
-        { symbol }
-    ].filter(item => Object.values(item)[0]);
 
-    if (typesCount === 0) {
-        return '';
+//random password function
+function randomPassword(length = 12) {
+    var chars = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+<>ABCDEFGHIJKLMNOP1234567890";
+    var pass = "";
+    for (var x = 0; x < length; x++) {
+        var i = Math.floor(Math.random() * chars.length);
+        pass += chars.charAt(i);
     }
-
-    //loop
-    for (let i = 0; i < length; i += typesCount) {
-        typesArr.forEach(type => {
-            const funcName = Object.keys(type)[0];
-            generatedPassword += randomFunction[funcName]();
-        });
-    }
-
-    const finalPassword = generatedPassword.slice(0, length);
-
-    return finalPassword;
+    return pass;
 }
 
-//Get random lowercase letter
-function randomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-//Get random uppercase letter
-function randomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-//Get random number
-function randomNumber() {
-    return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-//Get random symbol
-function randomSymbol() {
-    const symbols = '!@#$%^&*(){}[]=<>/,.'
-    return symbols[Math.floor(Math.random() * symbols.length)];
-}
+console.log(randomPassword());
 
