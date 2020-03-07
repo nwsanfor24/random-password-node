b45cf8d81b175f2c17fdb062bbe88e0cbb38f60d
Nathan Sanford <nwsanfor24@gmail.com>
2020-03-07 16:06:43 -0500

    random function generator

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ inquirer.prompt([
         name: 'password',
         choices: [
             {
-                name: 'lowercase'
+                name: 'lowercase',
             },
             {
                 name: 'uppercase'
@@ -24,6 +24,13 @@ inquirer.prompt([
     }
 ]).then(function (data) {
 
+    let userChoice = data.choices;
+    if(userChoice === 'lowercase') {
+        console.log(getRandomLower());
+    } else if (userChoice === 'uppercase') {
+        console.log(getRandomUpper());
+    }
+
     var filename = data.name + ".json";
 
     fs.writeFile(filename, JSON.stringify(data, null, '\t'), function (err) {
@@ -32,18 +39,41 @@ inquirer.prompt([
             return console.log(err);
         }
 
-        console.log("Success! Here is your random password " + randomPassword());
+        console.log("Success! Here is your random password: " + randomPassword());
     });
 
 });
 
-
-
-
-
 //random password function
-function randomPassword(length = 12) {
-    var chars = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+<>ABCDEFGHIJKLMNOP1234567890";
+
+function getRandomLower() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+
+
+function getRandomUpper() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+
+
+function getRandomNumber() {
+	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+
+
+function getRandomSymbol() {
+	const symbols = '!@#$%^&*(){}[]=<>/,.'
+	return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+function randomPassword(length = 10) {
+    var chars = "abcdefghijklmnopqrstuvwxyz";
+    var symbols = "!@#$%^&*()-+<>";
+    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numbers = "0123456789";
     var pass = "";
     for (var x = 0; x < length; x++) {
         var i = Math.floor(Math.random() * chars.length);
