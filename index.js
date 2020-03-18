6de0cd056c3f464e9f1d925dc99806e88ab6b1b9
Nathan Sanford <nwsanfor24@gmail.com>
2020-03-18 00:20:35 -0400

    took out question object and added it to inquirer prompt

--- index.js
+++ index.js
@@ -1,50 +1,49 @@
 var inquirer = require("inquirer");
 const Password = require("./pass");
 
-const questions = [{
-    type: 'input',
-    name: 'length',
-    message: "Select a number between 8 and 128",
-    validate: function (val) {
-        const numVal = parseInt(val.trim());
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'length',
+        message: "Select a number between 8 and 128",
+        validate: function (val) {
+            const numVal = parseInt(val.trim());
 
-        if (numval < 129 && numVal > 7) {
-            return true;
-        } else {
-            return error('WRONG. Enter a number between 8 and 128.');
-        }
-    },
-    type: 'checkbox',
-    name: 'characters',
-    message: "Select one or more types of characters for your password",
-    choices: [
-        {
-            name: 'lowercase'
+            if (numval < 129 && numVal > 7) {
+                return true;
+            } else {
+                return error('WRONG. Enter a number between 8 and 128.');
+            }
         },
-        {
-            name: 'uppercase'
-        },
-        {
-            name: 'number'
-        },
-        {
-            name: 'special'
-        }
-    ],
-    validate: (val) => {
-        const valueLength = val.length;
+        type: 'checkbox',
+        name: 'characters',
+        message: "Select one or more types of characters for your password",
+        choices: [
+            {
+                name: 'lowercase'
+            },
+            {
+                name: 'uppercase'
+            },
+            {
+                name: 'number'
+            },
+            {
+                name: 'special'
+            }
+        ],
+        validate: (val) => {
+            const valueLength = val.length;
 
-        if (val.length > 0) {
-            return true;
-        } else {
-            return error('How many times do I have to teach you this listen, old man? Select at least one or more characters');
+            if (val.length > 0) {
+                return true;
+            } else {
+                return error('How many times do I have to teach you this listen, old man? Select at least one or more characters');
+            }
         }
-    }
-}];
-
-inquirer.prompt(questions).then(({ length, characters}) => {
-    const generatedPassword = new Password(characters, length);
-    console.log("Success! Here is your password " + (`${generatedPassword.printPassword()}`));
-}).catch((error) => {
-    console.log(error(`Oh for Christ's sake, what now?`));
-});
+    }]).then(({length, characters}) => {
+        const generatedPassword = new Password(characters, length);
+        console.log(`Success! Here is your password ${generatedPassword.printPassword()}`);
+    }).catch((error) => {
+        console.log(error(`Oh for Christ's sake, what now!?`));
+    });
