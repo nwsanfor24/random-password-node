0011816408af5d93b12bf5d72655868c38e2bb26
Nathan Sanford <nwsanfor24@gmail.com>
2020-03-17 23:48:28 -0400

    Changing EVERYTHING

--- index.js
+++ index.js
@@ -2,87 +2,49 @@ var inquirer = require("inquirer");
 var fs = require('fs');
 
 const questions = [{
+    type: 'input',
+    name: 'length',
+    message: "Select a number between 8 and 128",
+    validate: function (val) {
+        const numVal = parseInt(val.trim());
+
+        if (numval < 129 && numVal > 7) {
+            return true;
+        } else {
+            return error('WRONG. Enter a number between 8 and 128.');
+        }
+    },
     type: 'checkbox',
-    name: 'password',
-    message: 'Select your password options',
+    name: 'characters',
+    message: "Select one or more types of characters for your password",
     choices: [
         {
-            name: 'lowercase',
-                validate: function(input) {
-                    var done = this.async();
-                    if (typeof input === 'lowercase') {
-                        done(getRandomLower(), true);
-                        return;
-                    } else {
-                        return "You can't do shit";
-                    }
-                }
-            },
-            {
-                name: 'uppercase',
-                validate: function (input) {
-                    var done = this.async();
-                    if (typeof input === 'uppercase') {
-                        done(getRandomUpper(), true);
-                        return;
-                    } else {
-                        return "YOU CAN'T DO SHIT";
-                    }
-                }
-            },
-            {
-                name: 'number',
-                validate: function(input) {
-                    var done = this.async();
-                    if (typeof input === 'number') {
-                        done(getRandomNumber(), true);
-                        return;
-                    } else {
-                        return "7336";
-                    }
-                }
-            },
-            {
-                name: 'symbol',
-                validate: function(input) {
-                    var done = this.async();
-                    if (typeof input === 'symbol') {
-                        done(getRandomSymbol(), true);
-                        return;
-                    } else {
-                        return "!@#$%^^&*";
-                    }
-                }
-            }
-    ]
+            name: 'lowercase'
+        },
+        {
+            name: 'uppercase'
+        },
+        {
+            name: 'number'
+        },
+        {
+            name: 'special'
+        }
+    ],
+    validate: (val) => {
+        const valueLength = val.length;
+
+        if (val.length > 0) {
+            return true;
+        } else {
+            return error('How many times do I have to teach you this listen, old man? Select at least one or more characters');
+        }
+    }
 }];
 
-inquirer.prompt(questions).then(answers => {
-    console.log(`Success, here is your random password: ${answers['password']}`);
+inquirer.prompt(questions).then(({ length, characters}) => {
+    const generatedPassword = new Password(characters, length);
+    console.log("Success! Here is your password " + (`${generatedPassword.printPassword()}`));
+}).catch((error) => {
+    console.log(error(`Oh for Christ's sake, what now?`));
 });
-
-//random password function
-
-function getRandomLower() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-
-
-function getRandomUpper() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-
-
-function getRandomNumber() {
-	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-
-
-function getRandomSymbol() {
-	const symbols = '!@#$%^&*(){}[]=<>/,.'
-	return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
