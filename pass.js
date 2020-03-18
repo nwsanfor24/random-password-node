834e3d4579df793adc1783a608bd67ad945e215d
Nathan Sanford <nwsanfor24@gmail.com>
2020-03-18 00:08:54 -0400

    Made pass.js

---
+++ pass.js
@@ -0,0 +1,62 @@
+const Character = require("./char");
+
+class Password {
+
+    constructor(characters, passLen) {
+        this.characters = characters;
+        this.numCharacterTypes = this.characters.length;
+        this.passLen = parseInt(passLen);
+        this.remainingCharacters = this.passLen - this.numCharacterTypes;
+        this.password = [];
+    }
+
+    generateCharacter(element) {
+        const newCharacterObj = new Character(element);
+        const newChar = newCharacterObj.finalPick();
+        this.password.push(newChar);
+    };
+
+    baseCharacter() {
+        this.characters.forEach(element => {
+            this.generateCharacter(element);
+        });
+        return this.password;
+    };
+
+    additionalCharacter() {
+        for (let i = 0; i < this.remainingCharacters; i++) {
+            const randomNumber = Math.floor((Math.random() * this.numCharacterTypes));
+            const randType = this.characters[randomNumber];
+            this.generateCharacter(randType);
+        };
+        return this.password;
+    };
+
+    finalPassArray() {
+        this.baseCharacter();
+        this.additionalCharacter();
+
+        let currentIndex = this.passLen;
+        let tempVal;
+        let randomIndex;
+
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            tempVal = this.password[currentIndex];
+            this.password[currentIndex] = this.password[randomIndex];
+            this.password[randomIndex] = tempVal;
+        }
+
+        return this.password;
+    }
+
+    printPassword() {
+        this.finalPassArray();
+        const finalPassword = this.password.join(``);
+        return finalPassword;
+    }
+}
+
+module.exports = Password;
